17ee697abb0a3c0769657828a865fb0faace67d5
Alexander Korobov <dktwho@mail.ru>
2023-01-24 11:22:10 +0300

    map of todolist and render

--- client/src/components/TodoList/TodoList.js
+++ client/src/components/TodoList/TodoList.js
@@ -4,12 +4,16 @@ import React from 'react'
 import Todo from '../Todo/Todo'
 
 const TodoList = ({todos}) => {
+
   return (
     <>
     <hr/>
   
     <ul className="list-group py-3">
-      <Todo/>
+      {todos.map((todo) => {
+        return  <Todo key={todo.id} title={todo.title}/>
+      })}
+     
     </ul>
     </>
   )
